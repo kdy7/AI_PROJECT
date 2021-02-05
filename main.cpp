c6d11c6ce2aa9de9655bb088731a517a9e08f112
Charlie Walker <ctw60@txstate.edu>
2021-02-05 14:36:27 -0600

    adding main file

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*---------------------------------------------------
+    Program written by Charles Walker, Dakota Ruhl, and Kalin Ybarra
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void treatment() {
+
+}
+
+void diagnosis() {
+
+}
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
