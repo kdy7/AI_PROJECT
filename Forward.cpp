859c24527620075464a170e7cdca34e222d745ed
kdy7 <76789535+kdy7@users.noreply.github.com>
2021-02-25 12:03:30 -0600

    Updated lists
    Updates the lists with our variables.

--- Forward.cpp
+++ Forward.cpp
@@ -27,12 +27,14 @@ int flag;
 vector<string> cndvar(10);
 //char varlt[10][3], /* variable list*/ clvarlt[40][3]; /* clause var list */
 vector<string> varlt(10);
-vector<string>clvarlt(4);
+vector<string> clvarlt(50);
 
 //char c[3], vp[3], /* condition variable */  v[3]; /*variable */
 string c, vp, v;
 //char fedint[10], interest[10], stock[10], dollar[10], fedmon[10];
-string fedint, interest, stock, dollar, fedmon;
+//string fedint, interest, stock, dollar, fedmon;
+int stage;
+string health, leukemia, radiation, chemo, stem;
 //char po[10], /* position */  qu[10]; /* qualify */
 string po, qu;
 //int instlt[10];         /* instantiated list*/
@@ -84,11 +86,12 @@ int main()
     //strcpy(varlt[3], "FM");
     //strcpy(varlt[4], "IN");
     //strcpy(varlt[5], "ST");
-    varlt[1] = "DO";
-    varlt[2] = "FT";
-    varlt[3] = "FM";
-    varlt[4] = "IN";
-    varlt[5] = "ST";
+    varlt[1] = "STA"; //STAGE
+    varlt[2] = "HEA"; //Health
+    varlt[3] = "LEU"; // Leukemia
+    varlt[4] = "RAD"; // Radiation
+    varlt[5] = "CHEM"; // Chemo
+    varlt[6] = "STEM"; // Stem cells
 
     printf("*** VARIABLE LIST ***\n");
     for (i=1;i < 11; i++)
@@ -111,12 +114,46 @@ int main()
     //strcpy(clvarlt[17], "FT");
     //strcpy(clvarlt[18], "FM");
     
-    clvarlt[1] = "IN";
-    clvarlt[5] = "IN";
-    clvarlt[9] = "DO";
-    clvarlt[13] = "DO";
-    clvarlt[17] = "FT";
-    clvarlt[18] = "FM";
+    clvarlt[1] = "STA";
+    clvarlt[2] = "HEA";
+    
+    clvarlt[4] = "STA";
+    clvarlt[5] = "HEA";
+    
+    clvarlt[8] = "STA";
+    clvarlt[9] = "LEU";
+    clvarlt[10] = "RAD";
+    
+    clvarlt[12] = "STA";
+    clvarlt[13] = "LEU";
+    clvarlt[14] = "RAD";
+    clvarlt[15] = "CHEM";
+    
+    clvarlt[18] = "STA";
+    clvarlt[19] = "LEU";
+    clvarlt[20] = "RAD";
+    clvarlt[21] = "CHEM";
+    clvarlt[22] = "STEM";
+    
+    clvarlt[24] = "STA";
+    clvarlt[25] = "LEU";
+    clvarlt[26] = "RAD";
+    clvarlt[27] = "CHEM";
+    clvarlt[28] = "STEM";
+    
+    clvarlt[30] = "STA";
+    clvarlt[31] = "LEU";
+    clvarlt[32] = "CHEM";
+    
+    clvarlt[36] = "STA";
+    clvarlt[37] = "LEU";
+    clvarlt[38] = "CHEM";
+    clvarlt[39] = "STEM";
+    
+    clvarlt[41] = "STA";
+    clvarlt[42] = "LEU";
+    clvarlt[43] = "CHEM";
+    clvarlt[44] = "STEM";
     
     printf("*** CLAUSE-VARIABLE LIST ***\n");
     for (i = 1; i < 9; i++)
@@ -193,37 +230,89 @@ int main()
                 s = 0;
                 /* sample IF-THEN statements from the position knowledge base */
 
-                switch(sn)
-                {
+                
+              // switch(sn)
+               // {
                     /* statement 1 */
                     /***** comment 1500 *****/
                 //case 1: if (strcmp(interest, "FALL") == 0) s=1;
-                    case 1: if (interest == "FALL") s=1;
-                    break;
+                 //   case 1: if (interest == "FALL") s=1;
+                   // break;
                     /* statement 2 */
                     /***** comment 1510 *****/
                 //case 2: if (strcmp(interest, "RISE") == 0) s=1;
-                    case 2: if (interest == "RISE") s=1;
-                    break;
+                    //case 2: if (interest == "RISE") s=1;
+                    //break;
                     /* statement 3 */
                     /***** comment 1540 *****/
                 //case 3: if (strcmp(dollar, "FALL") == 0) s=1;
-                    case 3: if (dollar == "FALL") s=1;
-                    break;
+                    //case 3: if (dollar == "FALL") s=1;
+                    //break;
                     /* statement 4 */
                     /***** comment 1550 *****/
                 //case 4: if (strcmp(dollar, "RISE") == 0) s=1;
-                    case 4: if (dollar == "RISE") s=1;
-                    break;
+                    //case 4: if (dollar == "RISE") s=1;
+                    //break;
                     /* statement 5 */
                 //case 5: if ((strcmp(fedint, "FALL") == 0) && (strcmp(fedmon, "ADD")) == 0) s=1;
-                    case 5: if ((fedint == "FALL") && (fedmon =="ADD")) s=1;
-                    break;
+                  //  case 5: if ((fedint == "FALL") && (fedmon =="ADD")) s=1;
+                   // break;
                     /* statement 6 */
                 //case 6: if ((strcmp(qu, "YES") == 0) && (gr >= 3.5) == 0) s=1;
-                    case 6: if((qu == "YES") && (gr>=3.5) == 0) s=1;
-                    break;
+                    //case 6: if((qu == "YES") && (gr>=3.5) == 0) s=1;
+                    //break;
                     /***** comment 1610 *****/
+                //}
+                
+                /*
+                switch(sn){
+                    case 1: if ((stage <= 1) && (health == "YES")) s=1;
+                        break;
+                    case 2: if ((stage <= 1) && (health == "NO")) s=1;
+                        break;
+                    case 3: if((stage > 1) && (leukemia == "NO") && (radiation == "YES")) s=1;
+                        break;
+                    case 4: if ((stage > 1) && (leukemia == "NO") && (radiation == "NO") && (chemo == "YES")) s=1;
+                        break;
+                    case 5: if ((stage > 1) && (leukemia == "NO") && (radiation == "NO") && (chemo == "NO") && (stem == "NO")) s=1;
+                        break;
+                    case 6: if ((stage > 1) && (leukemia == "NO") && (radiation == "NO") && (chemo == "NO") && (stem == "YES")) s=1;
+                        break;
+                    case 7: if( (stage > 1) && (leukemia == "YES") && (chemo == "NO") && (stem == "NO") ) s =1;
+                        break;
+                    case 8: if( (stage > 1) && (leukemia == "YES") && (chemo == "NO") && (stem == "YES") ) s =1;
+                        break;
+                        
+                }
+                 */
+                
+                //stage = 0 or stage = 1. 0 - surgery possible. 1 - no surgery possible
+                switch(sn){
+                    case 1: if(stage <= 1) s=1;
+                        break;
+                    case 2: if(stage > 1) s=1;
+                        break;
+                    case 3: if( health == "YES") s=1;
+                        break;
+                    case 4: if(health == "NO") s=1;
+                        break;
+                    case 5: if(leukemia == "YES") s=1;
+                        break;
+                    case 6: if(leukemia == "NO") s=1;
+                        break;
+                    case 7: if(chemo == "YES") s =1;
+                        break;
+                    case 8: if(chemo == "NO") s =1;
+                        break;
+                    case 9: if(radiation == "YES") s=1;
+                        break;
+                    case 10: if(radiation == "NO") s=1;
+                        break;
+                    case 11: if(stem == "YES") s=1;
+                        break;
+                    case 12: if(stem == "NO") s=1;
+                        break;
+                        
                 }
 
                 /* see if the THEN part should be inovked, i.e., s=1 */
@@ -244,64 +333,134 @@ int main()
      {
 
             /* invoke THEN part */
-            switch (sn)
-            {
+           // switch (sn)
+            //{
                 /*********** comment 1500 ***********/
                 /* put variable on the conclusion variable queue */
-            case 1:
+           // case 1:
                 //strcpy(stock, "RISE");
-                    stock = "RISE";
-                printf("ST=RISE\n");
+             //       stock = "RISE";
+               // printf("ST=RISE\n");
                 //strcpy(v, "ST");
-                v = "ST";
-                instantiate();
-                break;
+                //v = "ST";
+                //instantiate();
+                //break;
                 /*********** comment 1510 ***********/
                 /* put variable on the conclusion variable queue */
-            case 2:
+            //case 2:
                 //strcpy(stock, "FALL");
-                    stock = "FALL";
-                printf("ST=FALL\n");
+              //      stock = "FALL";
+               // printf("ST=FALL\n");
                 //strcpy(v, "ST");
-                v = "ST";
-                instantiate();
-                break;
+               // v = "ST";
+               // instantiate();
+              //  break;
                 /*********** comment 1540 ***********/
                 /* put variable on the conclusion variable queue */
-            case 3:
+           // case 3:
                 //strcpy(interest, "RISE");
-                    interest = "RISE";
-                printf("IN=RISE\n");
+                //    interest = "RISE";
+               // printf("IN=RISE\n");
                 //strcpy(v, "IN");
-                    v = "IN";
-                instantiate();
-                break;
+                //    v = "IN";
+                //instantiate();
+               // break;
                 /*********** comment 1550 ***********/
                 /* put variable on the conclusion variable queue */
-            case 4:
+           // case 4:
                // strcpy(interest, "FALL");
-                    interest = "FALL";
-                printf("IN=FALL\n");
+             //       interest = "FALL";
+             //   printf("IN=FALL\n");
                 //strcpy(v, "IN");
-                v = "IN";
-                instantiate();
-                break;
+               // v = "IN";
+               // instantiate();
+               // break;
                 /* put variable on the conclusion variable queue */
-            case 5:
+           // case 5:
                 //strcpy(interest, "FALL");
-                    interest = "FALL";
-                printf("IN=FALL\n");
+               //     interest = "FALL";
+               // printf("IN=FALL\n");
                 //strcpy(v, "IN");
-                    v = "IN";
-                instantiate();
-                break;
-            case 6:
+               //     v = "IN";
+                //instantiate();
+                //break;
+            //case 6:
                 //strcpy(po, "YES");
-                    po = "YES";
-                printf("PO=YES\n");
-                break;
+              //      po = "YES";
+              //  printf("PO=YES\n");
+              //
                 /*********** comment 1610 ***********/
-            }
+          // }
+         
+                //stage = 0 or stage = 1. 0 - surgery possible. 1 - no surgery possible
+                switch(sn){
+                    case 1:
+                        cout << "Stage = " << stage;
+                        stage = 0;
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 2:
+                        cout << "Stage = " << stage;
+                        stage = 1;
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 3:
+                        health = "YES";
+                        v = "IN";
+                        cout << "We recommend surgery.";
+                        //instantiate();
+                        break;
+                    case 4:
+                        health = "NO";
+                        v = "IN";
+                        cout << "We recomend drug treatment";
+                        //instantiate();
+                        break;
+                    case 5:
+                        leukemia = "YES";
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 6:
+                        leukemia = "NO";
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 7:
+                        chemo = "YES";
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 8:
+                        chemo = "NO";
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 9:
+                        radiation = "YES";
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 10:
+                        radiation = "NO";
+                        v = "IN";
+                        instantiate();
+                        break;
+                    case 11:
+                        stem = "YES";
+                        v = "IN";
+                        cout << "We reccomend stem cell treatment.";
+                        break;
+                    case 12:
+                        stem = "NO";
+                        v = "IN";
+                        cout << "We reccoment drug treatment.";
+                        break;
+                }
+         
+         
             f = sn + 1;
             //goto b496;
 
@@ -354,30 +513,33 @@ void check_instantiation()
         {
             /* input statements for sample position knowledge base */
         case 1:
-            printf("RISE OR FALL FOR DO? ");
+            printf("What stage is the cancer? ");
             //gets(dollar);
-                getline(cin, dollar);
+                cin >> stage;
             break;
         case 2:
-            printf("RISE OR FALL FOR FT? ");
+            printf("Are you healthy enough  for surgery?");
             //gets(fedint);
-                getline(cin, fedint);
+                getline(cin, health);
             break;
         case 3:
-            printf("ADD OR SUBTRACT FOR FM? ");
+            printf("Is the cancer Leukemia? ");
             //gets(fedmon);
-                getline(cin, fedmon);
+                getline(cin, leukemia);
                 break;
         case 4:
-            printf("RISE OR FALL FOR IN? ");
+            printf("Can you do radiation? ");
             //gets(interest);
-                getline(cin, interest);
+                getline(cin, radiation);
             break;
         case 5:
-            printf("RISE OR FALL FOR ST? ");
+            printf("Can you do chemo? ");
             //gets(stock);
-                getline(cin, stock);
+                getline(cin, chemo);
             break;
+            case 6: printf("Can you do stem cell treatment? " );
+                getline(cin, stem);
+                break;
         }
     }
     /* end of input statements for the position knowledge base */
@@ -436,3 +598,4 @@ void instantiate()
         bp=bp+1;
     }
 }
+
